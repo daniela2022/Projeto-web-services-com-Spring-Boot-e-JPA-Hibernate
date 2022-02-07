267d0339de1deb96f7a627d65c6e08a213eec35c
Daniela Pedon <danielapedon3@gmail.com>
2022-02-07 16:19:46 -0300

    Category

---
+++ src/main/java/com/educandoweb/course/resources/CategoryResource.java
@@ -0,0 +1,35 @@
+package com.educandoweb.course.resources;
+
+import com.educandoweb.course.entities.Category;
+import com.educandoweb.course.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController//retorna o objeto e os dados do obj são gravados diretamente na resposta HTTP com Json ou XML
+@RequestMapping(value = "/categories")//define a url que quando for requisitada chamara o metodo
+public class CategoryResource {
+
+    @Autowired
+    private CategoryService service;
+
+    @GetMapping//indicando que este metodo responde a requisição do tipo get do HTTP
+    public ResponseEntity<List<Category>> findAll(){
+        List<Category> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Category> findById(@PathVariable Long id){
+        Category obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
+
+
+}
